d35edcd5ea8fd94b8c2226686ffd4e4d2f5f3bbe
sxrgi0 <sergiofh.2002@gmail.com>
2025-04-10 16:35:58 +0200

    Implemented a search bar

--- app/src/main/java/com/example/superhero/activities/MainActivity.kt
+++ app/src/main/java/com/example/superhero/activities/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.example.superhero.activities
 
 import android.os.Bundle
+import android.view.Menu
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
+import androidx.appcompat.widget.SearchView.OnQueryTextListener
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.GridLayoutManager
@@ -10,6 +14,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.superhero.R
 import com.example.superhero.adapters.SuperheroAdapter
 import com.example.superhero.data.SuperHero
+import com.example.superhero.data.SuperHeroSearchResponse
 import com.example.superhero.utils.SuperHeroService
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -41,6 +46,27 @@ class MainActivity : AppCompatActivity() {
         searchSuperHero("a")
 
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.activity_main_menu, menu)
+
+        val menuItem = menu.findItem(R.id.menu_search)
+        val searchView = menuItem.actionView as SearchView
+
+        searchView.setOnQueryTextListener(object : OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                searchSuperHero(query)
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                return false
+            }
+
+        })
+
+        return true
     }
 
     fun searchSuperHero(query: String){
